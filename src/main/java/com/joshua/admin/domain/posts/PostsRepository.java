a51e3127611df5d5d7c7c507633f1bd6481b9cb6
joshuayun <eurojoshua@naver.com>
2020-02-29 23:24:42 +0900

    JPA 테스트 코드 작성

---
+++ src/main/java/com/joshua/admin/domain/posts/PostsRepository.java
@@ -0,0 +1,7 @@
+package com.joshua.admin.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+    //JpaRepository<Entity클래스, PK타입>
+}
---
+++ src/test/java/com/joshua/admin/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,21 @@
+package com.joshua.admin.web.dto;
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void lombok_exec_test(){
+        //given
+        String name = "test";
+        int amount = 1000;
+
+        //when
+        HelloResponseDto helloResponseDto = new HelloResponseDto(name, amount);
+
+        //then
+        assertThat(helloResponseDto.getName()).isEqualTo(name);
+        assertThat(helloResponseDto.getAmount()).isEqualTo(amount);
+    }
+}
